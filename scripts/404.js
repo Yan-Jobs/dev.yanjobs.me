637f44a822321418c9fd8e0b79ef8681c65ec03b
Yan-Jobs <yan@yanjobs.me>
2021-05-28 17:59:42 +0200

    :bug: Fix typed text

--- scripts/404.js
+++ scripts/404.js
@@ -4,17 +4,16 @@ const btn = document.querySelector(".btn");
 const l1 = document.querySelector(".l1");
 const l2 = document.querySelector(".l2");
 const btnContent = document.querySelector(".btn-Content");
+let opts = {
+  strings: ["404 | Page not found"],
+  typeSpeed: 20,
+  showCursor: false,
+};
+const typed = new Typed("#typedText", opts);
 window.addEventListener("load", () => {
   const TL = gsap.timeline({ paused: true });
   TL.from(logo, 0.4, { transform: "scale(0)", ease: "power2.out" })
     .from(l1, 1.5, { width: 0, ease: "power2.out" })
-    .from(l2, 1.5, { width: 0, ease: "power2.out" }, "-=2") ;
-  TL.play().then((play) => {
-    let opts = {
-      strings: ["404 | Page not found"],
-      typeSpeed: 20,
-      showCursor: false,
-    };
-
-    const typed = new Typed("#typedText", opts);
-})})
+    .from(l2, 1.5, { width: 0, ease: "power2.out" }, "-=2");
+  TL.play();
+});
